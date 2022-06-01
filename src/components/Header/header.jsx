06b182f64d8cf7b3ef3c5e507bada183a36cb5a1
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-06-01 18:55:31 +0530

    addition of header styling

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { StaticImage } from "gatsby-plugin-image";
+import "./header.scss";
 
 const Header = () => {
-  return <header></header>;
+  return (
+    <header className="page-header">
+      <div className="inner-container flex-container">
+        <div className="logo">
+          <StaticImage
+            className=""
+            src="https://res.cloudinary.com/dqove2qhg/image/upload/v1654088761/Upzyde/log2_csjfn8.png"
+            alt="Upzyde"
+            title="Upzyde"
+            placeholder="none"
+          />
+        </div>
+
+        <nav className="nav-links flex-container">
+          <div className="link">Home</div>
+          <div className="link">Developers</div>
+          <div className="link">Advertisers</div>
+          <div className="link">In Game Ads</div>
+          <div className="link">Contact Us</div>
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- src/components/Layout/layout.jsx
+++ src/components/Layout/layout.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Footer from "../Footer/footer";
+import Header from "../Header/header";
 import "./layout.scss";
 
 const Layout = ({ children }) => {
   return (
     <div className="page-layout">
+      <Header />
       <div className="page-content">{children}</div>
       <Footer />
     </div>

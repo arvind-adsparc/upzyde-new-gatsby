2138c9495f4f0f6ec8b04641bda68190692cf0e0
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-06-01 17:12:00 +0530

    addition of how it works section

---
+++ src/components/Works/works.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./works.scss";
+
+const Works = () => {
+  return (
+    <section className="works-container">
+      <h2>Here’s How it Works</h2>
+
+      <div className="content"></div>
+
+      <div className="cta">
+        <button className="btn">Get Started</button>
+      </div>
+    </section>
+  );
+};
+
+export default Works;
--- src/pages/developers/index.js
+++ src/pages/developers/index.js
@@ -3,11 +3,13 @@ import { overviewData, partnersdata } from "./data";
 import Layout from "../../components/Layout/layout";
 import OtherPageOverview from "../../components/Overview/OtherPageOverview/otherPageOverview";
 import Partners from "../../components/Partners/partners";
+import Works from "../../components/Works/works";
 
 const Developers = () => {
   return (
     <Layout>
       <OtherPageOverview {...overviewData} />
+      <Works />
       <Partners {...partnersdata} />
     </Layout>
   );

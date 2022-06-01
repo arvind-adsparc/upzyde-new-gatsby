c6c8744f97c60e1dd3b621264d9741e7d4c20d0d
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-06-01 12:03:40 +0530

    footer background image addition

--- src/pages/developers/index.js
+++ src/pages/developers/index.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Layout from "../../components/Layout/layout";
 
 const Developers = () => {
-  return <section>Page content goes here</section>;
+  return (
+    <Layout>
+      <section>Page content goes here</section>
+    </Layout>
+  );
 };
 
 export default Developers;

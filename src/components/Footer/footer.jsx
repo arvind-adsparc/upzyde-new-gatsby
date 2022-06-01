322709559d7afe6ef73d419c5ff58dd0c14c6f77
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-06-01 12:01:25 +0530

    addition of footer component

---
+++ src/components/Footer/footer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./footer.scss";
+
+const Footer = () => {
+  return <footer className="footer-section">footer goes here</footer>;
+};
+
+export default Footer;
--- src/components/Layout/layout.jsx
+++ src/components/Layout/layout.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Footer from "../Footer/footer";
 
 const Layout = ({ children }) => {
   return (
     <div>
       <div className="page-content">{children}</div>
+      <Footer />
     </div>
   );
 };

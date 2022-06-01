fe72788cca69407afa86698a6a7eeb4f68d886f7
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-06-01 14:14:40 +0530

    layout style sheet connected

---
+++ src/components/Overview/OtherPapeOverview/otherPageOverview.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const OtherPageOverview = () => {
+  return (
+    <section className="overview-container">
+      <div className="inner-container">Content goes here</div>
+    </section>
+  );
+};
+
+export default OtherPageOverview;
--- src/pages/developers/index.js
+++ src/pages/developers/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Layout from "../../components/Layout/layout";
+import OtherPageOverview from "../../components/Overview/OtherPapeOverview/otherPageOverview";
 
 const Developers = () => {
   return (
     <Layout>
-      <section>Page content goes here</section>
+      <OtherPageOverview />
     </Layout>
   );
 };

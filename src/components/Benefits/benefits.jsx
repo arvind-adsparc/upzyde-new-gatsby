7755e01ee155bd398c5a226be27124e421fdcc97
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-06-01 17:33:09 +0530

    benefits section styling

--- src/components/Benefits/benefits.jsx
+++ src/components/Benefits/benefits.jsx
@@ -4,13 +4,24 @@ import "./benefits.scss";
 const Benefits = ({ heading, subHeading, content, cta }) => {
   return (
     <section className="benefits-container">
-      <h2>{heading}</h2>
-      <h3>{subHeading}</h3>
+      <div className="inner-container">
+        <h2>{heading}</h2>
+        <h3>{subHeading}</h3>
 
-      <div className="content-container"></div>
+        <div className="content-container ">
+          {content.map((info) => {
+            return (
+              <div key={info.title} className="card">
+                <div className="card-title">{info.title}</div>
+                <div className="card-txt">{info.text}</div>
+              </div>
+            );
+          })}
+        </div>
 
-      <div className="cta">
-        <button className="btn">{cta.text}</button>
+        <div className="cta">
+          <button className="btn">{cta.text}</button>
+        </div>
       </div>
     </section>
   );

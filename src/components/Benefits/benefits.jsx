b989aae7220d82b93bea39e41b057c9a6fc36431
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-06-01 18:15:14 +0530

    benefits background image fix

--- src/components/Benefits/benefits.jsx
+++ src/components/Benefits/benefits.jsx
@@ -1,29 +1,36 @@
 import React from "react";
+import Lazyload from "react-lazyload";
 import "./benefits.scss";
 
-const Benefits = ({ heading, subHeading, content, cta }) => {
+const Benefits = ({ heading, subHeading, content, backgroundImage, cta }) => {
   return (
-    <section className="benefits-container">
-      <div className="inner-container">
-        <h2>{heading}</h2>
-        <h3>{subHeading}</h3>
+    <Lazyload once>
+      <section
+        className="benefits-container"
+        style={{ backgroundImage: `url(${backgroundImage})` }}
+      >
+        <div></div>
+        <div className="section-content inner-container">
+          <h2>{heading}</h2>
+          <h3>{subHeading}</h3>
 
-        <div className="content-container ">
-          {content.map((info) => {
-            return (
-              <div key={info.title} className="card">
-                <div className="card-title">{info.title}</div>
-                <div className="card-txt">{info.text}</div>
-              </div>
-            );
-          })}
-        </div>
+          <div className="content-container ">
+            {content.map((info) => {
+              return (
+                <div key={info.title} className="card">
+                  <div className="card-title">{info.title}</div>
+                  <div className="card-txt">{info.text}</div>
+                </div>
+              );
+            })}
+          </div>
 
-        <div className="cta">
-          <button className="btn">{cta.text}</button>
+          <div className="cta">
+            <button className="btn">{cta.text}</button>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </Lazyload>
   );
 };
 

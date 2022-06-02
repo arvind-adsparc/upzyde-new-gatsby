b47db6eb1c69e21d6e11db227c35ff4b52ee73e6
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-06-02 17:11:55 +0530

    additon of advertisers page

---
+++ src/pages/advertisers/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Layout from "../../components/Layout/layout";
+
+const Advertisers = () => {
+  return <Layout>Advertisers</Layout>;
+};
+
+export default Advertisers;

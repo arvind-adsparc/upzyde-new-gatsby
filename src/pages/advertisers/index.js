5d9a1cb3b708174f99785f8c64e0305cc6da4e25
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-06-02 17:54:01 +0530

    advertisers hero section

--- src/pages/advertisers/index.js
+++ src/pages/advertisers/index.js
@@ -1,8 +1,13 @@
 import React from "react";
+import AdvertisersHero from "../../components/Hero/advertisers";
 import Layout from "../../components/Layout/layout";
 
 const Advertisers = () => {
-  return <Layout page="advertisers">Advertisers</Layout>;
+  return (
+    <Layout page="advertisers">
+      <AdvertisersHero />
+    </Layout>
+  );
 };
 
 export default Advertisers;

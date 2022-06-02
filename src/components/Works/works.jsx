072d5a8c9d990ce1d5966d14d94be4829cb590a6
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-06-02 11:03:16 +0530

    addition of dummy video thumbnail

--- src/components/Works/works.jsx
+++ src/components/Works/works.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { StaticImage } from "gatsby-plugin-image";
 import "./works.scss";
 
 const Works = () => {
@@ -6,7 +7,14 @@ const Works = () => {
     <section className="works-container">
       <h2>Here’s How it Works</h2>
 
-      <div className="content"></div>
+      <div className="content">
+        <StaticImage
+          src="https://res.cloudinary.com/dqove2qhg/image/upload/v1654147756/Upzyde/video_dummy-1_1_wvvxjf.webp"
+          alt="videp"
+          title="video"
+          placeholder="none"
+        />
+      </div>
 
       <div className="cta">
         <button className="btn">Get Started</button>

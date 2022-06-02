b9e23fc45f212375d181cfea0dd5689ec103a53a
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-06-02 17:59:44 +0530

    addition of advertiser overview section

---
+++ src/components/data/advertisers/data.js
@@ -0,0 +1,58 @@
+export const overviewData = {
+  content:
+    "<div> <p>Explore the S.M.A.R.T ways to connect to your audience via native ad placements blended intelligently into gameplay.</p>  <p>Experience a cohesive advertising campaign across multiple in-game & in-stream ad formats (display/video/audio).</p> </div>",
+
+  cta: {
+    text: "Start Now",
+    link: "",
+  },
+};
+
+export const partnersdata = {
+  heading: "Trusted Partners",
+  brandLogos:
+    "https://res.cloudinary.com/dqove2qhg/image/upload/v1654076189/Upzyde/brand_logos_-img-1_miri5w.webp",
+};
+
+export const benefitsData = {
+  backgroundImage:
+    "https://res.cloudinary.com/dqove2qhg/image/upload/v1654083824/Upzyde/benfits_bg-1_rtp3sw.webp",
+  heading: "Benefits for App Developers",
+  subHeading: "Why Choose Upzyde?",
+  content: [
+    {
+      title: "Effective Rendering",
+      text: "Dynamic rendering function for perfectly matched ad formats into the gaming environment",
+    },
+    {
+      title: "Creative Control",
+      text: "Enhanced inventory management related to data, content, ad formats, pricing, and more",
+    },
+    {
+      title: "Easy Integration",
+      text: "One simple to use drag-n-drop integration across several platforms",
+    },
+    {
+      title: "Accurate Reporting",
+      text: "Real-time reporting, tracking, & audits for better optimization",
+    },
+    {
+      title: "Premium Partnering",
+      text: "Get partnered with top-tier agencies & advertisers",
+    },
+    {
+      title: "Unity Verified",
+      text: "Our in-game ad solution is Unity verified so it’s trustworthy on a global scale",
+    },
+  ],
+  cta: {
+    text: "Get Started",
+    link: "",
+  },
+};
+
+// export const formatsData = {
+//   backgroundImage:
+//     "https://res.cloudinary.com/dqove2qhg/image/upload/v1654148784/Upzyde/ad_format_1_sc2zew.webp",
+//   heading: "AD FORMATS",
+// };
--- src/pages/advertisers/index.js
+++ src/pages/advertisers/index.js
@@ -1,11 +1,14 @@
 import React from "react";
-import AdvertisersHero from "../../components/Hero/advertisers";
+import { overviewData } from "../../components/data/advertisers/data";
 import Layout from "../../components/Layout/layout";
+import AdvertisersHero from "../../components/Hero/advertisers";
+import OtherPageOverview from "../../components/Overview/OtherPageOverview/otherPageOverview";
 
 const Advertisers = () => {
   return (
     <Layout page="advertisers">
       <AdvertisersHero />
+      <OtherPageOverview {...overviewData} />
     </Layout>
   );
 };

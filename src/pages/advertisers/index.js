202c632fb59536659ad307b4da5403f9a0eaf18a
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-06-02 18:09:18 +0530

    addition of how it works section

--- src/pages/advertisers/index.js
+++ src/pages/advertisers/index.js
@@ -3,12 +3,14 @@ import { overviewData } from "../../components/data/advertisers/data";
 import Layout from "../../components/Layout/layout";
 import AdvertisersHero from "../../components/Hero/advertisers";
 import OtherPageOverview from "../../components/Overview/OtherPageOverview/otherPageOverview";
+import Works from "../../components/Works/works";
 
 const Advertisers = () => {
   return (
     <Layout page="advertisers">
       <AdvertisersHero />
       <OtherPageOverview {...overviewData} />
+      <Works />
     </Layout>
   );
 };

d3c48be0b3969a58079d0ee7448df2ea4e2a0a0a
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-06-02 16:20:31 +0530

    footer newslette styling

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -9,14 +9,53 @@ const Footer = () => {
       <div className="inner-container grid-container">
         <div className="newsletter-section">
           <div className="mail-us">
-            <h3>Mail Us</h3>
+            <h3>Mail us</h3>
+            <div className="border-red"></div>
+            <div className="content">
+              <a className="mail-txt" href="mailto:up@upzyde.com">
+                up@upzyde.com
+              </a>
+            </div>
           </div>
 
           <div className="form">
-            <h3>Newsletter</h3>
+            <h3 className="color-red">Newsletter</h3>
           </div>
 
-          <div className="follow-us">Follow us</div>
+          <div className="follow-us">
+            <h3>Follow us</h3>
+            <div className="border-red"></div>
+
+            <div className="content">
+              <div className="social-link">
+                <StaticImage
+                  className=""
+                  src="https://res.cloudinary.com/dqove2qhg/image/upload/v1654166514/Upzyde/twit1_axsy40.webp"
+                  alt="Twitter"
+                  title="Twitter"
+                  placeholder="none"
+                />
+              </div>
+              <div className="social-link">
+                <StaticImage
+                  className=""
+                  src="https://res.cloudinary.com/dqove2qhg/image/upload/v1654166516/Upzyde/fb1_ogopuq.webp"
+                  alt="Facebook"
+                  title="Facebook"
+                  placeholder="none"
+                />
+              </div>
+              <div className="social-link">
+                <StaticImage
+                  className=""
+                  src="https://res.cloudinary.com/dqove2qhg/image/upload/v1654166515/Upzyde/insta1_ouhjrb.webp"
+                  alt="Instagram"
+                  title="Instagram"
+                  placeholder="none"
+                />
+              </div>
+            </div>
+          </div>
         </div>
         <div className="quick-links">
           <div className="logo">

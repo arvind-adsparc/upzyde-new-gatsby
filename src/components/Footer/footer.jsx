d4933968685e2311ae5924a5281b5b0bf6043ef5
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-06-02 15:54:23 +0530

    footer alignment implementaion

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,13 +1,37 @@
 import React from "react";
+import { StaticImage } from "gatsby-plugin-image";
+
 import "./footer.scss";
 
 const Footer = () => {
   return (
     <footer className="footer-section">
-      <div className="newsletter-section"></div>
-      <div className="quick-links"></div>
-      <div className="copyright-section inner-container">
-        <div>2022 © Copyright. All rights reserved. Privacy Policy</div>
+      <div className="inner-container grid-container">
+        <div className="newsletter-section">
+          <div className="mail-us">
+            <h3>Mail Us</h3>
+          </div>
+
+          <div className="form">
+            <h3>Newsletter</h3>
+          </div>
+
+          <div className="follow-us">Follow us</div>
+        </div>
+        <div className="quick-links">
+          <div className="logo">
+            <StaticImage
+              className=""
+              src="https://res.cloudinary.com/dqove2qhg/image/upload/v1654088761/Upzyde/log2_csjfn8.png"
+              alt="Upzyde"
+              title="Upzyde"
+              placeholder="none"
+            />
+          </div>
+        </div>
+        <div className="copyright-section ">
+          <div>2022 © Copyright. All rights reserved. Privacy Policy</div>
+        </div>
       </div>
     </footer>
   );

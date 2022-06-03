b77f558a79b6651a6608413035e1175b63d32108
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-06-03 14:02:03 +0530

    ad formats slider implementation

--- src/components/Formats/formats.jsx
+++ src/components/Formats/formats.jsx
@@ -4,14 +4,6 @@ import { Carousel } from "antd";
 import { StaticImage } from "gatsby-plugin-image";
 import "./formats.scss";
 
-const contentStyle = {
-  height: "160px",
-  color: "#fff",
-  lineHeight: "160px",
-  textAlign: "center",
-  background: "#364d79",
-};
-
 const Formats = () => {
   return (
     <Lazyload once>
@@ -20,15 +12,21 @@ const Formats = () => {
 
         <div className="inner-container">
           <div className="slider-container">
-            <Carousel dotPosition="right">
-              <div>
-                <h3 style={contentStyle}>1</h3>
+            <Carousel autoplay effect="fade" dotPosition="right">
+              <div className="slider">
+                <img
+                  src="https://res.cloudinary.com/dqove2qhg/image/upload/v1654243593/Upzyde/iphone-_1_w43h6z.webp"
+                  alt=""
+                  placeholder="none"
+                />
               </div>
-              <div>
-                <h3 style={contentStyle}>2</h3>
-              </div>
-              <div>
-                <h3 style={contentStyle}>3</h3>
+
+              <div className="slider">
+                <img
+                  src="https://res.cloudinary.com/dqove2qhg/image/upload/v1654245035/Upzyde/iphone-empty-1_1_asqoxs.webp"
+                  alt=""
+                  placeholder="none"
+                />
               </div>
             </Carousel>
           </div>

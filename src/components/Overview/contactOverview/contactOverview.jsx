a79333d0d2d2bfc0ad702c76a413dd35f9b12722
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-06-03 17:00:13 +0530

    addition of contact overview section

---
+++ src/components/Overview/contactOverview/contactOverview.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./styles.scss";
+
+const ConactOverview = () => {
+  return (
+    <section className="contact-overview inner-container">
+      <div className="form">
+        <h2>Get in Touch</h2>
+      </div>
+
+      <div className="location">
+        <h2>Location</h2>
+      </div>
+    </section>
+  );
+};
+
+export default ConactOverview;
--- src/pages/contact-us/index.js
+++ src/pages/contact-us/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ContactUsHero from "../../components/Hero/contactUs";
 import Layout from "../../components/Layout/layout";
+import ConactOverview from "../../components/Overview/contactOverview/contactOverview";
 
 const ContactUs = () => {
   return (
     <Layout page="contact">
       <ContactUsHero />
+      <ConactOverview />
     </Layout>
   );
 };

ecaa6cd480fdff2d13fe50366c243a69dc3b21f7
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-06-03 18:12:55 +0530

    contact us hero section responsive ness

--- src/components/Hero/contactUs/index.jsx
+++ src/components/Hero/contactUs/index.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { StaticImage } from "gatsby-plugin-image";
+import "./styles.scss";
 
 const ContactUsHero = () => {
   return (
-    <section className="advertisers-hero container">
+    <section className="contactus-hero container">
       <div className="hero-image">
         <StaticImage
           src="https://res.cloudinary.com/dqove2qhg/image/upload/v1654172465/Upzyde/banner_image_1_nzglwu.webp"

e0db91c9bcfa404f99106e10635f73eb4e7cc14c
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-06-05 11:49:15 +0530

    react-lazyload removal

--- src/components/Benefits/benefits.jsx
+++ src/components/Benefits/benefits.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Lazyload from "react-lazyload";
 import "./benefits.scss";
 
 const Benefits = ({
@@ -11,33 +10,31 @@ const Benefits = ({
   cta,
 }) => {
   return (
-    <Lazyload once>
-      <section
-        className="benefits-container"
-        style={{ backgroundImage: `url(${backgroundImage})` }}
-      >
-        <div></div>
-        <div className="section-content inner-container">
-          <h2>{heading}</h2>
-          <h3>{subHeading}</h3>
+    <section
+      className="benefits-container"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    >
+      <div></div>
+      <div className="section-content inner-container">
+        <h2>{heading}</h2>
+        <h3>{subHeading}</h3>
 
-          <div className={`content-container ${columns}`}>
-            {content.map((info) => {
-              return (
-                <div key={info.title} className="card">
-                  <div className="card-title">{info.title}</div>
-                  <div className="card-txt">{info.text}</div>
-                </div>
-              );
-            })}
-          </div>
-
-          <div className="cta">
-            <button className="btn">{cta.text}</button>
-          </div>
+        <div className={`content-container ${columns}`}>
+          {content.map((info) => {
+            return (
+              <div key={info.title} className="card">
+                <div className="card-title">{info.title}</div>
+                <div className="card-txt">{info.text}</div>
+              </div>
+            );
+          })}
         </div>
-      </section>
-    </Lazyload>
+
+        <div className="cta">
+          <button className="btn">{cta.text}</button>
+        </div>
+      </div>
+    </section>
   );
 };
 
--- src/components/Formats/formats.jsx
+++ src/components/Formats/formats.jsx
@@ -1,54 +1,51 @@
 import React from "react";
-import Lazyload from "react-lazyload";
 import { Carousel } from "antd";
 import { StaticImage } from "gatsby-plugin-image";
 import "./formats.scss";
 
 const Formats = () => {
   return (
-    <Lazyload once>
-      <section className="formats-container">
-        <h2>Ad Formats</h2>
+    <section className="formats-container">
+      <h2>Ad Formats</h2>
 
-        <div className="inner-container">
-          <div className="slider-container">
-            <Carousel autoplay effect="fade" dotPosition="right">
-              <div className="slider">
-                <img
-                  src="https://res.cloudinary.com/dqove2qhg/image/upload/v1654243593/Upzyde/iphone-_1_w43h6z.webp"
-                  alt=""
-                  placeholder="none"
-                />
-              </div>
-
-              <div className="slider">
-                <img
-                  src="https://res.cloudinary.com/dqove2qhg/image/upload/v1654245035/Upzyde/iphone-empty-1_1_asqoxs.webp"
-                  alt=""
-                  placeholder="none"
-                />
-              </div>
-            </Carousel>
-          </div>
-
-          <div className="content-container">
-            <div className="content-txt">
-              Our non-intrusive ad formats integrate IAB-supported ad placements
-              for display, audio & video, custom ads, and numerous streaming
-              prospects.
-            </div>
-
-            <div className="content-images">
-              <StaticImage
-                src="https://res.cloudinary.com/dqove2qhg/image/upload/v1654151197/Upzyde/Group_1_ota528.webp"
+      <div className="inner-container">
+        <div className="slider-container">
+          <Carousel autoplay effect="fade" dotPosition="right">
+            <div className="slider">
+              <img
+                src="https://res.cloudinary.com/dqove2qhg/image/upload/v1654243593/Upzyde/iphone-_1_w43h6z.webp"
                 alt=""
                 placeholder="none"
               />
             </div>
+
+            <div className="slider">
+              <img
+                src="https://res.cloudinary.com/dqove2qhg/image/upload/v1654245035/Upzyde/iphone-empty-1_1_asqoxs.webp"
+                alt=""
+                placeholder="none"
+              />
+            </div>
+          </Carousel>
+        </div>
+
+        <div className="content-container">
+          <div className="content-txt">
+            Our non-intrusive ad formats integrate IAB-supported ad placements
+            for display, audio & video, custom ads, and numerous streaming
+            prospects.
+          </div>
+
+          <div className="content-images">
+            <StaticImage
+              src="https://res.cloudinary.com/dqove2qhg/image/upload/v1654151197/Upzyde/Group_1_ota528.webp"
+              alt=""
+              placeholder="none"
+            />
           </div>
         </div>
-      </section>
-    </Lazyload>
+      </div>
+    </section>
   );
 };
 

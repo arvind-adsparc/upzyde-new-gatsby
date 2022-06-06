9658c364d6d4f7cb5ce8b5b58049f427370e860e
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-06-06 13:23:39 +0530

    addition of seo on advertisers page

--- src/pages/in-game-ads/video/index.js
+++ src/pages/in-game-ads/video/index.js
@@ -3,6 +3,7 @@ import HelpingPublisher from "../../../components/HelpingPublishers";
 import VideoAdHero from "../../../components/Hero/inGameAds/video";
 import Layout from "../../../components/Layout/layout";
 import OtherPageOverview from "../../../components/Overview/OtherPageOverview/otherPageOverview";
+import Seo from "../../../components/Seo/seo";
 
 export const overviewData = {
   content:
@@ -17,6 +18,11 @@ export const overviewData = {
 const VideoAds = () => {
   return (
     <Layout page="inGameAds">
+      <Seo
+        url="https://res.cloudinary.com/dqove2qhg/image/upload/v1654495670/Upzyde/in_game-1_banner-1_1_vydfe2.webp"
+        title=" Video - In Game Ads | Upzyde"
+        description="With Upzyde’s comprehensive in-game monetization solutions, we help publishers grow revenues, without compromising on player experience. Work with numerous demand sources and ad networks across the world to maximize the value of every impression."
+      />
       <VideoAdHero />
       <OtherPageOverview {...overviewData} />
       <HelpingPublisher />

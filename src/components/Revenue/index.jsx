d4291fac78f154f1cc1947642bc18ab02ce634e1
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-06-06 15:16:54 +0530

    revenue section complete

--- src/components/Revenue/index.jsx
+++ src/components/Revenue/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { StaticImage } from "gatsby-plugin-image";
+// import { StaticImage } from "gatsby-plugin-image";
 import IncreaseRevenueForm from "../Forms/increaseRevenue";
 import "./styles.scss";
 
@@ -7,18 +7,19 @@ const Revenue = () => {
   return (
     <section className="revenue-container inner-container">
       <div className="form-container">
-        <StaticImage
+        {/* <StaticImage
           src="https://res.cloudinary.com/dqove2qhg/image/upload/v1654507449/Upzyde/increase_your_revenue_1_wox80x.webp"
           alt=""
           placeholder="none"
-        />
+        /> */}
 
         <div className="form-content">
           <h2>
-            Crafting goal-oriented solutions for publishers across the globe
+            Crafting goal-oriented solutions for publishers{" "}
+            <span className="sentence-break">across the globe</span>
           </h2>
 
-          <p>Join Upzyde and start making the most of your games today</p>
+          <p>Join Upzyde and start making the most of your games today.</p>
 
           <IncreaseRevenueForm />
         </div>

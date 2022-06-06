2fe3301e4f87dc938e8744887bc9441d6ba46fdf
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-06-06 16:05:32 +0530

    addition of mobile hero image for in game video page

--- src/components/Hero/inGameAds/video.jsx
+++ src/components/Hero/inGameAds/video.jsx
@@ -6,11 +6,20 @@ const VideoAdHero = () => {
   return (
     <section className="videoAd-hero container">
       <div className="hero-image">
-        <StaticImage
-          src="https://res.cloudinary.com/dqove2qhg/image/upload/v1654495670/Upzyde/in_game-1_banner-1_1_vydfe2.webp"
-          alt=""
-          placeholder="none"
-        />
+        <div className="mobile">
+          <StaticImage
+            src="https://res.cloudinary.com/dqove2qhg/image/upload/v1654511218/Upzyde/in_game-1_banner-1_1_1_p0bhms.png"
+            alt=""
+            placeholder="none"
+          />
+        </div>
+        <div className="desktop">
+          <StaticImage
+            src="https://res.cloudinary.com/dqove2qhg/image/upload/v1654495670/Upzyde/in_game-1_banner-1_1_vydfe2.webp"
+            alt=""
+            placeholder="none"
+          />
+        </div>
       </div>
     </section>
   );

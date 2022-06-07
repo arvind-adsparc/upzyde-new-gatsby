bb2a89d731fc675437ff8a0a244db56a42b9c325
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-06-07 11:09:34 +0530

    gatsby static image on advertiser hero image

--- src/components/Hero/advertisers/index.jsx
+++ src/components/Hero/advertisers/index.jsx
@@ -1,15 +1,14 @@
 import React from "react";
-// import { StaticImage } from "gatsby-plugin-image";
+import { StaticImage } from "gatsby-plugin-image";
 
 const AdvertisersHero = () => {
   return (
     <section className="advertisers-hero container">
       <div className="hero-image">
-        <img
+        <StaticImage
           src="https://res.cloudinary.com/dqove2qhg/image/upload/v1654172465/Upzyde/banner_image_1_nzglwu.webp"
           alt=""
-          loading="lazy"
-          // placeholder="none"
+          placeholder="none"
         />
       </div>
     </section>

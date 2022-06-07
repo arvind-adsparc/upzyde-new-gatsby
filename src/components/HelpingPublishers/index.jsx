6ffc1cc8ac8ef1e4e0b819146a9450869c1a3c59
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-06-07 11:55:24 +0530

    addition of slider content

--- src/components/HelpingPublishers/index.jsx
+++ src/components/HelpingPublishers/index.jsx
@@ -15,13 +15,16 @@ const HelpingPublisher = () => {
 
       <div className="inner-container">
         <div className="slider-container">
-          <div className="prev-arrow" onClick={() => slider.current.prev()}>
+          <div
+            className="prev-arrow arrow"
+            onClick={() => slider.current.prev()}
+          >
             Prev
           </div>
 
           <Carousel effect="fade" dots={false} ref={slider}>
             <div className="slider">
-              <div className="slider-title">Scalable Ad Solutions:</div>
+              <div className="slider-title">Scalable Ad Solutions</div>
               <div className="slider-description">
                 Our exemplary tools and strategies ensure your goals are
                 accomplished. Be it revenue generation or user acquisition, our
@@ -29,15 +32,39 @@ const HelpingPublisher = () => {
               </div>
             </div>
             <div className="slider">
-              <div className="slider-title">TRestScalable Ad Solutions:</div>
+              <div className="slider-title">Earn More With Your Games</div>
               <div className="slider-description">
-                Our exemplary tools and strategies ensure your goals are
-                accomplished. Be it revenue generation or user acquisition, our
-                turnkey solutions have the entire ad ecosystem covered.
+                Upzyde’s monetization solutions help you make the most of your
+                games and convert them into revenue-generating businesses. With
+                innovative ad formats and advanced technology, we ensure you
+                earn higher CPMs for your ad inventory.
+              </div>
+            </div>
+
+            <div className="slider">
+              <div className="slider-title">Know Your User</div>
+              <div className="slider-description">
+                With real-time reporting and tracking the Upzyde dashboard
+                enables publishers to deep-dive into analytics about user
+                engagement, ad performance, and more. Know your user in detail
+                and optimize your ads on the go to provide an unmatched gaming
+                experience.
+              </div>
+            </div>
+            <div className="slider">
+              <div className="slider-title">Ads For Everyone</div>
+              <div className="slider-description">
+                Not every game is the same, and not every user should see the
+                same ad. Our innovative monetization strategies and diverse ad
+                offerings help you choose ad types best suited to your
+                playerbase.
               </div>
             </div>
           </Carousel>
-          <div className="next-arrow" onClick={() => slider.current.next()}>
+          <div
+            className="next-arrow arrow"
+            onClick={() => slider.current.next()}
+          >
             Next
           </div>
         </div>

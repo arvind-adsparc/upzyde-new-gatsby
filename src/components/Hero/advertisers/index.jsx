7fb43f4194a73e3b6c00a3199748cafeeb22203e
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-06-07 11:01:07 +0530

    advertisers page hero image lazy loading removed

--- src/components/Hero/advertisers/index.jsx
+++ src/components/Hero/advertisers/index.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import { StaticImage } from "gatsby-plugin-image";
+// import { StaticImage } from "gatsby-plugin-image";
 
 const AdvertisersHero = () => {
   return (
     <section className="advertisers-hero container">
       <div className="hero-image">
-        <StaticImage
+        <img
           src="https://res.cloudinary.com/dqove2qhg/image/upload/v1654172465/Upzyde/banner_image_1_nzglwu.webp"
           alt=""
-          placeholder="none"
+          // placeholder="none"
         />
       </div>
     </section>

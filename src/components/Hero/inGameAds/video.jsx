aa03ea8927d2a623a001637f90ad57b287fbc656
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-06-08 11:41:51 +0530

    in game video page hero width correction

--- src/components/Hero/inGameAds/video.jsx
+++ src/components/Hero/inGameAds/video.jsx
@@ -4,22 +4,13 @@ import "./styles.scss";
 
 const VideoAdHero = () => {
   return (
-    <section className="videoAd-hero container">
+    <section className="videoAd-hero">
       <div className="hero-image">
-        <div className="mobile">
-          <StaticImage
-            src="https://res.cloudinary.com/dqove2qhg/image/upload/v1654518936/Upzyde/in_game-1_banner-1_1_1_1_n0u85o.png"
-            alt=""
-            placeholder="none"
-          />
-        </div>
-        <div className="desktop">
-          <StaticImage
-            src="https://res.cloudinary.com/dqove2qhg/image/upload/v1654495670/Upzyde/in_game-1_banner-1_1_vydfe2.webp"
-            alt=""
-            placeholder="none"
-          />
-        </div>
+        <StaticImage
+          src="https://res.cloudinary.com/dqove2qhg/image/upload/v1654495670/Upzyde/in_game-1_banner-1_1_vydfe2.webp"
+          alt=""
+          placeholder="none"
+        />
       </div>
     </section>
   );

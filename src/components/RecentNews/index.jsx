3d2c55df068ff322f5b1006fd59a478d841707d8
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-06-18 13:05:02 +0530

    home page video fit for mobile

--- src/components/RecentNews/index.jsx
+++ src/components/RecentNews/index.jsx
@@ -8,31 +8,71 @@ const RecentNews = () => {
       <h2>Recent News</h2>
 
       <div className="cards">
-        {[1, 2, 3].map((info) => {
-          return (
-            <div key={info} className="card">
-              <div className="card-content">
-                <StaticImage
-                  src="https://res.cloudinary.com/dqove2qhg/image/upload/v1655123590/Upzyde/recent_bg2_1_srpz5h.png"
-                  alt=""
-                  placeholder=""
-                  width={300}
-                />
-                <h3>News Title</h3>
-                <p>
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit
-                  asperiores deserunt, placeat illum eius veniam provident
-                  ratione libero! Odio fugit quibusdam distinctio obcaecati,
-                  excepturi nemo quasi? Voluptates commodi veritatis modi.
-                </p>
+        <div className="card">
+          <div className="card-content">
+            <StaticImage
+              src="https://res.cloudinary.com/dqove2qhg/image/upload/v1655537520/Upzyde/recent_bg1_2_zyfqfa.png"
+              alt=""
+              placeholder=""
+              width={300}
+            />
+            <h3>News Title</h3>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit
+              asperiores deserunt, placeat illum eius veniam provident ratione
+              libero! Odio fugit quibusdam distinctio obcaecati, excepturi nemo
+              quasi? Voluptates commodi veritatis modi.
+            </p>
 
-                <div className="read-more">
-                  <button>Read More</button>
-                </div>
-              </div>
+            <div className="read-more">
+              <button>Read More</button>
             </div>
-          );
-        })}
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="card-content">
+            <StaticImage
+              src="https://res.cloudinary.com/dqove2qhg/image/upload/v1655123590/Upzyde/recent_bg2_1_srpz5h.png"
+              alt=""
+              placeholder=""
+              width={300}
+            />
+            <h3>News Title</h3>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit
+              asperiores deserunt, placeat illum eius veniam provident ratione
+              libero! Odio fugit quibusdam distinctio obcaecati, excepturi nemo
+              quasi? Voluptates commodi veritatis modi.
+            </p>
+
+            <div className="read-more">
+              <button>Read More</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="card-content">
+            <StaticImage
+              src="https://res.cloudinary.com/dqove2qhg/image/upload/v1655537520/Upzyde/recent_bg1_2_zyfqfa.png"
+              alt=""
+              placeholder=""
+              width={300}
+            />
+            <h3>News Title</h3>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit
+              asperiores deserunt, placeat illum eius veniam provident ratione
+              libero! Odio fugit quibusdam distinctio obcaecati, excepturi nemo
+              quasi? Voluptates commodi veritatis modi.
+            </p>
+
+            <div className="read-more">
+              <button>Read More</button>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );

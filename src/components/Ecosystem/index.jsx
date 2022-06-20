74a65b43c862ac7c2787ce76e61415bc10fec8fc
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-06-20 13:21:25 +0530

    ecosystem image change

--- src/components/Ecosystem/index.jsx
+++ src/components/Ecosystem/index.jsx
@@ -7,16 +7,14 @@ const Ecosystem = () => {
     <section className="ecosystem-container">
       <h2>Ecosystem</h2>
 
-      <StaticImage
-        src="https://res.cloudinary.com/dqove2qhg/image/upload/v1655709888/Upzyde/ecosystem_1_zdcsfk.png"
-        // src="https://res.cloudinary.com/dqove2qhg/image/upload/v1655709768/Upzyde/ecosystem_sqbazq.webp"
-        // src="https://res.cloudinary.com/dqove2qhg/image/upload/v1654843326/Upzyde/ecosystem_1_rrjdgv.webp"
-        // src="https://res.cloudinary.com/dqove2qhg/image/upload/v1655210615/Upzyde/ecosystem_y7j1nl.png"
-        alt=""
-        placeholder="none"
-        width={800}
-        // height={350}
-      />
+      <div className="image-div">
+        <img
+          src="https://res.cloudinary.com/dqove2qhg/image/upload/v1655709768/Upzyde/ecosystem_sqbazq.webp"
+          alt=""
+          placeholder="none"
+          // width={800}
+        />
+      </div>
     </section>
   );
 };

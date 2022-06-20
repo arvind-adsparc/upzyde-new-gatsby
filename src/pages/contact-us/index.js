84eee54f5605f02d6326907d0a401e03af6a77aa
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-06-20 17:09:29 +0530

    contact us page title

--- src/pages/contact-us/index.js
+++ src/pages/contact-us/index.js
@@ -2,10 +2,16 @@ import React from "react";
 import ContactUsHero from "../../components/Hero/contactUs";
 import Layout from "../../components/Layout/layout";
 import ConactOverview from "../../components/Overview/contactOverview/contactOverview";
+import Seo from "../../components/Seo/seo";
 
 const ContactUs = () => {
   return (
     <Layout page="contact">
+      <Seo
+        url="https://res.cloudinary.com/dqove2qhg/image/upload/v1654087901/Upzyde/Banner_image_1_bshcup.webp"
+        title=" Contact Us | Upzyde"
+        description="Use Upzyde’s advanced tech to maximize revenue with in-app bidding. Our innovative ad monetization features safeguard your brand, improve UX, and serve you with maximum return on inventory."
+      />
       <ContactUsHero />
       <ConactOverview />
     </Layout>

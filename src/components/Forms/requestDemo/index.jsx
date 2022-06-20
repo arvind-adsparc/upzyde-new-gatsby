6d1f8bb2303f41381a80d3070aab7c10cebb7a41
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-06-20 11:23:43 +0530

    request demo form api integration

--- src/components/Forms/requestDemo/index.jsx
+++ src/components/Forms/requestDemo/index.jsx
@@ -1,12 +1,54 @@
-import React from "react";
-import { Form, Input, Select } from "antd";
+import React, { useState } from "react";
+import axios from "axios";
+import { Form, Input, Select, Spin } from "antd";
+import { LoadingOutlined } from "@ant-design/icons";
+import { API_URL } from "../../../utlis/apiURL";
 import "./styles.scss";
 
 const { Option } = Select;
 
+const antIcon = (
+  <LoadingOutlined
+    style={{
+      fontSize: 24,
+    }}
+    spin
+  />
+);
+
 const RequestDemoForm = () => {
-  const onFinish = (values) => {
-    console.log("Success:", values);
+  const [form] = Form.useForm();
+  const [loading, setLoading] = useState(false);
+  const [status, setStatus] = useState("");
+  const onFinish = async (values) => {
+    // console.log("Success:", values);
+
+    setLoading(true);
+
+    try {
+      const data = {
+        firstName: values.firstName,
+        lastName: values.lastName,
+        email: values.email,
+        format: values.format,
+        page: "Advertisers Page",
+      };
+
+      const headers = {
+        "Content-Type": "application/json",
+      };
+      const res = await axios.post(`${API_URL}/request-demo`, data, {
+        headers,
+      });
+
+      setLoading(false);
+      setStatus("success");
+      form.resetFields();
+    } catch (err) {
+      setLoading(false);
+      setStatus("error");
+      console.log("err", err);
+    }
   };
 
   const onFinishFailed = (errorInfo) => {
@@ -17,27 +59,36 @@ const RequestDemoForm = () => {
   return (
     <section className="rqDemo-container request-demo">
       <div className="inner-container">
-        <Form name="" onFinish={onFinish} onFinishFailed={onFinishFailed}>
+        <Form
+          form={form}
+          name=""
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+        >
           <div className="formItem-container">
             <Form.Item
-              name="first-name"
+              name="firstName"
               rules={[
                 {
                   required: true,
                   message: "Please input your first name!",
                 },
               ]}
+              hasFeedback
+              validateStatus={status}
             >
               <Input placeholder="First Name" />
             </Form.Item>
             <Form.Item
-              name="last-name"
+              name="lastName"
               rules={[
                 {
                   required: true,
                   message: "Please input your last name!",
                 },
               ]}
+              hasFeedback
+              validateStatus={status}
             >
               <Input placeholder="Last Name" />
             </Form.Item>
@@ -53,6 +104,8 @@ const RequestDemoForm = () => {
                   message: "Please input your email!",
                 },
               ]}
+              hasFeedback
+              validateStatus={status}
             >
               <Input placeholder="Email" />
             </Form.Item>
@@ -65,6 +118,8 @@ const RequestDemoForm = () => {
                   message: "Please select an Ad Format!",
                 },
               ]}
+              hasFeedback
+              validateStatus={status}
             >
               <Select placeholder="Ad Format">
                 <Option value="adFormat">Ad Format</Option>
@@ -75,7 +130,11 @@ const RequestDemoForm = () => {
 
           <Form.Item>
             <div className="cta">
-              <button className="btn">Request Demo</button>
+              {loading ? (
+                <Spin indicator={antIcon} />
+              ) : (
+                <button className="btn">Request Demo</button>
+              )}
             </div>
           </Form.Item>
         </Form>

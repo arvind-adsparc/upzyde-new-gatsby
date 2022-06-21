d6ad13b19bba1286043a4b7cdac6031463757443
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-06-21 12:53:42 +0530

    button hover change

--- src/components/Works/works.jsx
+++ src/components/Works/works.jsx
@@ -9,7 +9,7 @@ const Works = () => {
     <section className="works-container">
       <h2>Here’s How it Works</h2>
 
-      <div className="content test">
+      <div className="content">
         <StaticImage
           src="https://res.cloudinary.com/dqove2qhg/image/upload/v1654147756/Upzyde/video_dummy-1_1_wvvxjf.webp"
           alt="videp"

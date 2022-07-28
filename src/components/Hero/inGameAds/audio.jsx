ad6575b39ccefa42fbd9a7f49792ce967f0a9c6a
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-07-28 15:20:16 +0530

    basic changes

--- src/components/Hero/inGameAds/audio.jsx
+++ src/components/Hero/inGameAds/audio.jsx
@@ -1,17 +1,16 @@
 import React from "react";
-import { StaticImage } from "gatsby-plugin-image";
 import "./styles.scss";
 
 const AudioAdHero = () => {
   return (
     <section className="videoAd-hero">
       <div className="hero-image">
-        <img
+        {/* <img
           src="https://res.cloudinary.com/dqove2qhg/image/upload/v1658922581/Upzyde/banner_image_1_fg5elt.png"
           alt=""
           placeholder="none"
           objectFit="fill"
-        />
+        /> */}
       </div>
 
       <div className="content">
